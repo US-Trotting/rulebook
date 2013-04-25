fbe895dd3ee17eaaa0c1b778bcb50bd09a834f3f
Derek Brigner <derek.brigner@ustrotting.com>
2013-04-25 09:09:22 -0400

    complete swipe addition and remove unneeded files

--- js/ustarulebook.js
+++ js/ustarulebook.js
@@ -4,3 +4,17 @@ $(document).bind("mobileinit", function(){
 	$.mobile.pushStateEnabled = false; 
 	$.mobile.transitionFallbacks='none'; 
 });
+
+$( document ).on( "pageinit", "[data-role='page']", function() {
+	var page = "#" + $( this ).attr( "id" )
+		// Get the filename of the next page that we stored in the data-next attribute
+		next = $( this ).jqmData( "next" ),
+		// Get the filename of the previous page that we stored in the data-prev attribute
+		prev = $( this ).jqmData( "prev" );
+	$( page ).on( "swipeleft", function() {
+		$.mobile.changePage( next + ".html" );
+	});
+	$( page ).on( "swiperight", function() {
+		$.mobile.changePage( prev + ".html", { reverse: true } );
+	});
+});

71008d5a1ca30f21f65ddfbac3de0a3132cf4cb3
unknown <derek.brigner@.USTROTTING.LOCAL>
2013-05-07 19:44:37 -0400

    test

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
-$(document).bind("mobileinit", function(){
-	$.mobile.defaultPageTransition = "none"; 
-	$.mobile.phonegapNavigationEnabled = true; 
-	$.mobile.pushStateEnabled = false; 
-	$.mobile.transitionFallbacks='none'; 
-});
+var app = {
+	initialize: function() {
+		$.mobile.allowCrossDomainPages = true;
+		$.mobile.defaultPageTransition = "none"; 
+		$.mobile.phonegapNavigationEnabled = true; 
+		$.mobile.pushStateEnabled = false; 
+		$.mobile.transitionFallbacks='none';
+	}
+};
 
 $('div.ui-page')
 	.live('swipeleft', function(){

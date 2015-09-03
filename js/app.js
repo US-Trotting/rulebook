01ab21e9ec9d596a93bd77dfc19ef5c05d296b6e
Derek Brigner <derekbrigner@gmail.com>
2015-09-03 12:06:00 -0400

    Update javascript and swiping

--- js/app.js
+++ js/app.js
@@ -1,35 +1,38 @@
-var app = {
-	initialize: function() {
-		var self = this;
-		
+(function($,undefined) {
+	
+	$( document ).on( "mobileinit", function() {
 		$.mobile.allowCrossDomainPages = true;
 		$.mobile.defaultPageTransition = "none"; 
 		$.mobile.phonegapNavigationEnabled = true; 
 		$.mobile.pushStateEnabled = false; 
 		$.mobile.transitionFallbacks='none';
-	}
-};
-
-$(document).on("pageinit", "[data-role='page'].page", function(){
-	var page = "#" + $( this ).attr( "id" ),
+	});
+	
+	$( document ).on( "pagecreate", "[data-role='page'].page", function() {
 		// Get the filename of the next page that we stored in the data-next attribute
-		next = $( this ).jqmData( "next" ),
+		var	next = $(this).jqmData("next");
 		// Get the filename of the previous page that we stored in the data-prev attribute
-		prev = $( this ).jqmData( "prev" );
-	
-	var alertStr = page.toString() + ' ' + next.toString() + ' ' + prev.toString();
-
-	if(next){
-		$(page).on("swipeleft", function(){
-			$.mobile.changePage( next + '.html', {transition: "slide"});
+		var prev = $(this).jqmData("prev");
+		
+		$("body").unbind("swipeleft");
+		$("body").unbind("swiperight");
+		
+		
+		$("body").on("swipeleft", function(){
+			if(next){
+				$("body").pagecontainer("change", next + ".html", {transition: "slide"});
+			}
 		});
-	};
-
-	if(prev){
-		$(page).on("swiperight", function(){
-			$.mobile.changePage( prev + '.html', {transition: "slide", reverse: true});
+		
+		$("body").on("swiperight", function(){
+			if(prev){
+				$("body").pagecontainer("change", prev + ".html", {transition: "slide", reverse: true});
+			}
 		});
-	};
+	});
 	
-	alert(alertStr);
-});
+})(jQuery);
+
+
+
+

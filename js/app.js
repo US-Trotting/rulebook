383da3cf79b8c9e8397486ce86cb8e5d95e6ff45
Derek Brigner <derekbrigner@gmail.com>
2015-10-01 09:23:45 -0400

    Fix javascript swipe error

--- js/app.js
+++ js/app.js
@@ -10,12 +10,16 @@ $(document).on("pagecreate", function(event) {
 	var hammertime = Hammer(event.target);
 	
 	hammertime.on("swipeleft", function(e) {
-		var nextPage = $(event.target).jqmData("next") + ".html";
-		$("body").pagecontainer("change", nextPage, {transition: "slide"});
+		var nextPage = $(event.target).jqmData("next");
+		if ( nextPage) {
+			$("body").pagecontainer("change", nextPage + ".html", {transition: "slide"});
+		}
 	});
 	
 	hammertime.on("swiperight", function(e) {
-		var prevPage = $(event.target).jqmData("prev") + ".html";
-		$("body").pagecontainer("change", prevPage, {transition: "slide", reverse: true});
+		var prevPage = $(event.target).jqmData("prev");
+		if ( prevPage ) {
+			$("body").pagecontainer("change", prevPage + ".html", {transition: "slide", reverse: true});
+		}
 	});
 });
